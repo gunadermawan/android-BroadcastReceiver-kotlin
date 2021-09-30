2c6fd4b45aefb4093087cacc389dbabe67e56d1d
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-09-30 10:51:53 +0700

    membuat activity sms

---
+++ app/src/main/java/com/gunder/mybroadcastreceiver/SmsReceiverActivity.kt
@@ -0,0 +1,11 @@
+package com.gunder.mybroadcastreceiver
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SmsReceiverActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sms_receiver)
+    }
+}
