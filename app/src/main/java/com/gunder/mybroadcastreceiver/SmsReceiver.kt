4a9b340bb273d219d33f1205a170a4e4e21598f5
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-09-30 20:39:21 +0700

    menambhkan kode untuk SmsReceiver

--- app/src/main/java/com/gunder/mybroadcastreceiver/SmsReceiver.kt
+++ app/src/main/java/com/gunder/mybroadcastreceiver/SmsReceiver.kt
@@ -3,11 +3,48 @@ package com.gunder.mybroadcastreceiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.telephony.SmsMessage
+import android.util.Log
+import java.lang.Exception
+import java.util.*
 
 class SmsReceiver : BroadcastReceiver() {
+    companion object {
+        private val TAG = SmsReceiver::class.java.simpleName
+    }
 
     override fun onReceive(context: Context, intent: Intent) {
         // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
-        TODO("SmsReceiver.onReceive() is not implemented")
+        val bundle = intent.extras
+        try {
+            if (bundle != null){
+//               Bundle dengan key pdus adalah standar dari system
+                val pdusObj = bundle.get("pdus") as Array<*>
+                for (aPdusObj in pdusObj){
+                    val currennMessage = getIncomeMessage(aPdusObj as Any, bundle)
+                    val senderNum = currennMessage.displayOriginatingAddress
+                    val message = currennMessage.displayMessageBody
+                    Log.d(TAG, "SenderNum: $senderNum; message: $message")
+                    val showSmsIntent = Intent(context, SmsReceiverActivity::class.java)
+                    showSmsIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                    showSmsIntent.putExtra(SmsReceiverActivity.EXTRA_SMS_NO, senderNum)
+                    showSmsIntent.putExtra(SmsReceiverActivity.EXTRA_SMS_MESSAGE, message)
+                    context.startActivity(showSmsIntent)
+
+                }
+            }
+        } catch (e:Exception){
+            Log.d(TAG, "Exception smsReceiver $e")
+        }
+    }
+    private fun getIncomeMessage(aObject: Any, bundle: Bundle): SmsMessage{
+        val currentSms: SmsMessage
+        val format = bundle.getString("format")
+        currentSms = if (Build.VERSION.SDK_INT >= 23){
+            SmsMessage.createFromPdu(aObject as ByteArray, format)
+        } else SmsMessage.createFromPdu(aObject as ByteArray)
+        return currentSms
     }
 }
