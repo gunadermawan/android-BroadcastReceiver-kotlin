7a921c4971549259a4e596b501fe559b822f41df
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-10-01 11:02:14 +0700

    membuat kelas DownloadService

---
+++ app/src/main/java/com/gunder/mybroadcastreceiver/DownloadService.kt
@@ -0,0 +1,9 @@
+package com.gunder.mybroadcastreceiver
+
+import android.content.Intent
+import androidx.core.app.JobIntentService
+
+class DownloadService: JobIntentService() {
+    override fun onHandleWork(intent: Intent) {
+    }
+}
--- app/src/main/java/com/gunder/mybroadcastreceiver/MainActivity.kt
+++ app/src/main/java/com/gunder/mybroadcastreceiver/MainActivity.kt
@@ -9,22 +9,33 @@ import com.gunder.mybroadcastreceiver.databinding.ActivityMainBinding
 import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-//    binding
+    //    binding
     private var binding: ActivityMainBinding? = null
+
     companion object {
+        const val ACTION_DOWNLOAD_STATUS = "download_status"
         private const val SMS_REQUEST_CODE = 101
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
 
         binding?.btnPermission?.setOnClickListener(this)
+        binding?.btnDownload?.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
-        when{
-            v?.id == R.id.btn_permission -> PermissionManager.check(this, android.Manifest.permission.RECEIVE_SMS, SMS_REQUEST_CODE)
+        when (v?.id) {
+            R.id.btn_permission -> PermissionManager.check(
+                this,
+                android.Manifest.permission.RECEIVE_SMS,
+                SMS_REQUEST_CODE
+            )
+            R.id.btn_download -> {
+
+            }
         }
     }
 
@@ -34,14 +45,15 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == SMS_REQUEST_CODE){
+        if (requestCode == SMS_REQUEST_CODE) {
             when {
                 grantResults[0] == PackageManager.PERMISSION_GRANTED -> Toast.makeText(
                     this,
                     "SMS receiver permission diterima",
                     Toast.LENGTH_SHORT
                 ).show()
-                else -> Toast.makeText(this, "SMS receiver permission ditolak", Toast.LENGTH_SHORT).show()
+                else -> Toast.makeText(this, "SMS receiver permission ditolak", Toast.LENGTH_SHORT)
+                    .show()
             }
         }
     }
